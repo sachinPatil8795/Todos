e0887904b42fb1303498b821f81eec22479dd56e
sachin_sp95 <sachinsp8795@gmail.com>
2023-08-13 18:01:31 +0530

    dynamic styling

---
+++ src/MyComponents/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import "./CourseInput.css";
+import Button from "../../UI/Button/Button";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "#ccc",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button
+        style={{ background: !isValid ? "lightcoral" : "#8b005d" }}
+        type="submit"
+      >
+        Add Goal
+      </Button>
+    </form>
+  );
+};
+
+export default CourseInput;
---
+++ src/MyComponents/UI/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Button.css";
+
+const Button = (props) => {
+  return (
+    <button type={props.type} style={props.style} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
